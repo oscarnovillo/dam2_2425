cfa8e2dcbea6c44a0c7bd8c6d6e1c1ac1a8c1f11
oscar <oscar@oscar.com>
2024-11-04 08:26:10 +0100

    navigation example

---
+++ moviles/Navigation/app/src/main/java/com/example/navigation/ui/personas/BlankFragment.kt
@@ -0,0 +1,31 @@
+package com.example.navigation.ui.personas
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.navigation.R
+import com.example.navigation.databinding.FragmentBlankBinding
+import com.example.navigation.databinding.FragmentOtraBinding
+
+class BlankFragment : Fragment() {
+
+    private var _binding: FragmentBlankBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentBlankBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding){
+
+        }
+    }
+}
