acc002a397ec2eccc8ef5574df8dda4c23df910f
oscarnovillo <oscar.novillo@gmail.com>
2025-01-10 00:26:06 +0100

    dfsdfsdf

--- moviles/ViewModel Navigation/app/build.gradle.kts
+++ moviles/ViewModel Navigation/app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.example.viewmodel"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.viewmodel"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
