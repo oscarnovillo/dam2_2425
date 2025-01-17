b75caad035fe172985bad4786d5657d97bc6e2be
oscar <oscar@oscar.com>
2025-01-17 11:49:54 +0100

    spring

---
+++ psp/backendSpring/backendSpring/src/test/java/org/example/backendspring/BackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backendspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
